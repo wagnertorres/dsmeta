1f0b9f841067a5cafd03ac167f32ba3ba7770118
Wagner Torres <wtorres@ciandt.com>
2022-09-10 10:34:35 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,11 +20,15 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dataMin = minDate.toISOString().slice(0, 10);
+        const dataMax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dataMin}&maxDate=${dataMax}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, [])
+    }, [minDate, maxDate])
 
     return (
         <div className="dsmeta-card">
